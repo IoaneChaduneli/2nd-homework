5d04e8e79a7254457161353960a4724cb478311f
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-10 23:51:38 +0400

    make a dictionary

---
+++ dict.py
@@ -0,0 +1,13 @@
+Phrases ={
+    'Mother Teresa' : '"Spread love everywhere you go. Let no one ever come to you without leaving happier."',
+    'Franklin D. Roosevelt' : '"When you reach the end of your rope, tie a knot in it and hang on."',
+    'Benjamin Franklin': '"Tell me and I forget. Teach me and I remember. Involve me and I learn."',
+    'Abraham Lincoln' : '"In the end, it is not the years in your life that count. It is the life in your years."',
+    'Thomas A. Edison' : '"Many of life is failures are people who did not realize how close they were to success when they gave up"'
+}
+
+
+author = input("Enter author: ")  
+
+
+print(Phrases[author])
